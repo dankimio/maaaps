073ea0f839f13bc3f8750ac170616f6b66150666
Dan Kim <itsdanya@gmail.com>
2023-01-22 17:59:25 +0000

    Add Google Maps JS loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { Loader } from '@googlemaps/js-api-loader'
+
+const loader = new Loader({
+  apiKey: "AIzaSyDjyIM1XPwAW252iSF9AKQ9bUTwwzMtrLI",
+  version: "weekly",
+  libraries: ["places"]
+});
+
+const mapOptions = {
+  center: {
+    lat: 0,
+    lng: 0
+  },
+  zoom: 4
+};
+
+// Promise
+loader
+  .load()
+  .then((google) => {
+    new google.maps.Map(document.getElementById("map"), mapOptions);
+  })
+  .catch(e => {
+    // do something
+  });
 </script>
 
 <template>
