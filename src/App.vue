297cb1d79bfed24106e5860337afe76d08f3167b
Dan Kim <itsdanya@gmail.com>
2023-01-22 18:03:33 +0000

    Style Google Maps

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import { Loader } from '@googlemaps/js-api-loader'
+import styles from './assets/styles'
 
 const loader = new Loader({
   apiKey: "AIzaSyDjyIM1XPwAW252iSF9AKQ9bUTwwzMtrLI",
@@ -9,11 +10,14 @@ const loader = new Loader({
 });
 
 const mapOptions = {
-  center: {
-    lat: 0,
-    lng: 0
-  },
-  zoom: 4
+  center: { lat: 55.7558, lng: 37.6173 },
+  zoom: 10,
+  streetViewControl: false,
+  zoomControl: false,
+  mapTypeControl: false,
+  fullscreenControl: false,
+  keyboardShortcuts: false,
+  styles: styles
 };
 
 // Promise
@@ -22,14 +26,12 @@ loader
   .then((google) => {
     new google.maps.Map(document.getElementById("map"), mapOptions);
   })
-  .catch(e => {
-    // do something
+  .catch((e) => {
+    console.log(e);
   });
 </script>
 
 <template>
-  <HelloWorld msg="Hello, world" />
-
   <div class="h-screen w-screen flex flex-col items-center justify-center">
     <input type="text" name="name" id="" class="mb-8 p-2 h-10 border rounded">
     <div id="map" style="width: 75%; height: 75%;"></div>
