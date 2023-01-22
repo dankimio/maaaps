d21dc91da77eb24262fddbb8811d9529621ea43c
Dan Kim <itsdanya@gmail.com>
2023-01-22 17:57:52 +0000

    Sun 22 Jan 2023 17:57:52 WET

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <HelloWorld msg="Hello, world" />
+
+  <div class="h-screen w-screen flex flex-col items-center justify-center">
+    <input type="text" name="name" id="" class="mb-8 p-2 h-10 border rounded">
+    <div id="map" style="width: 75%; height: 75%;"></div>
+  </div>
 </template>
 
 <style scoped>
+
 </style>
