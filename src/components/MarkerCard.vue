3166c49cec0a43bb46588b69f69ff7b716d416bf
Dan Kim <itsdanya@gmail.com>
2023-01-23 23:24:20 +0000

    Mon 23 Jan 2023 23:24:20 WET

--- src/components/MarkerCard.vue
+++ src/components/MarkerCard.vue
@@ -5,7 +5,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex flex-col bg-neutral-50 rounded-lg py-4 px-6 transition break-words">
+  <div class="flex flex-col hover:bg-neutral-50 rounded-lg py-4 px-6 transition break-words">
     <p class="mb-1">{{ marker.name }}</p>
     <p class="text-sm text-neutral-800">{{ marker.formatted_address }}</p>
   </div>
