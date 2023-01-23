3eb15e4d488d0d5eaee29aaa32783c9c6fc499b6
Dan Kim <itsdanya@gmail.com>
2023-01-23 20:41:32 +0000

    Mon 23 Jan 2023 20:41:32 WET

--- src/App.vue
+++ src/App.vue
@@ -32,27 +32,29 @@ function onInput() {
 </script>
 
 <template>
-  <div class="container mx-auto py-10">
-    <div class="relative mb-8 flex">
-      <input
-        type="text" name="name" id="" class=" p-2 h-10 border rounded"
-        @input="onInput"
-        v-model="query"
-        autocomplete="off">
+  <div class="container mx-auto py-10 grid grid-cols-12">
+    <div class="col-span-8">
+      <div class="relative mb-8 flex">
+        <input
+          type="text" name="name" id="" class=" p-2 h-10 border rounded"
+          @input="onInput"
+          v-model="query"
+          autocomplete="off">
 
-      <!-- <div v-if="results" class="absolute z-10 bg-white rounded p-2" style="top: calc(100% + 12px)">
-        <div v-for="result in results" :key="result" @click="onClick(result)">
-          {{ result.name }}
-        </div>
-      </div> -->
+        <!-- <div v-if="results" class="absolute z-10 bg-white rounded p-2" style="top: calc(100% + 12px)">
+          <div v-for="result in results" :key="result" @click="onClick(result)">
+            {{ result.name }}
+          </div>
+        </div> -->
+      </div>
+
+      <GoogleMap
+        :apiPromise="apiPromise"
+        :center="moscow"
+        style="width: 100%; height: 500px"
+        v-bind="mapOptions"
+      >
+      </GoogleMap>
     </div>
-
-    <GoogleMap
-      :apiPromise="apiPromise"
-      :center="moscow"
-      style="width: 100%; height: 400px"
-      v-bind="mapOptions"
-    >
-    </GoogleMap>
   </div>
 </template>
