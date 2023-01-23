c69671f7dac788ec82a2fed6e77f6f57643708cd
Dan Kim <itsdanya@gmail.com>
2023-01-23 23:38:58 +0000

    Mon 23 Jan 2023 23:38:58 WET

--- src/components/MarkerCard.vue
+++ src/components/MarkerCard.vue
@@ -1,12 +1,32 @@
 <script setup>
-defineProps({
-  marker: Object
+import { computed } from 'vue'
+
+const props = defineProps({
+  marker: {
+    type: Object,
+    required: true
+  }
+})
+
+const googleMapsUrl = computed(() => {
+  const params = {
+    api: '1',
+    query: props.marker.name,
+    query_place_id: props.marker.place_id
+  }
+  const urlSearchParams = new URLSearchParams(params).toString()
+  return `https://www.google.com/maps/search/?${urlSearchParams}`
 })
 </script>
 
 <template>
   <div class="flex flex-col hover:bg-neutral-50 rounded-lg py-4 px-6 transition break-words">
+    <!-- {{ marker }} -->
     <p class="mb-1">{{ marker.name }}</p>
     <p class="text-sm text-neutral-800">{{ marker.formatted_address }}</p>
+
+    <a :href="googleMapsUrl" target="_blank">Google Maps</a>
+
+    <!-- https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJw91Uj19KtUYRWvKcoPb43jU -->
   </div>
 </template>
