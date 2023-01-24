5d5a4063417d82659c9f68295eb042e6d4e0c134
Dan Kim <itsdanya@gmail.com>
2023-01-24 00:00:20 +0000

    Tue 24 Jan 2023 00:00:20 WET

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@ const onInput = debounce(() => {
   <div class="container mx-auto py-10">
     <div class="mb-8 relative flex">
       <input type="text" name="name" id="" class="p-2 h-10 border rounded" @input="onInput" v-model="query"
-      autocomplete="off">
+        autocomplete="off">
 
       <div v-if="searchResults.length" class="absolute z-10 bg-white rounded p-2" style="top: calc(100% + 12px)">
         <div v-for="result in searchResults" :key="result" @click="onClick(result)">
@@ -89,8 +89,7 @@ const onInput = debounce(() => {
 
     <div class="grid grid-cols-12 gap-x-8">
       <div class="col-span-8">
-
-        <GoogleMap :center="moscow" style="width: 100%; height: 500px" v-bind="mapOptions" ref="map">
+        <GoogleMap :center="moscow" style="width: 100%; height: 500px" v-bind="mapOptions" ref="map" class="rounded-lg overflow-hidden">
           <Marker v-for="marker in markers" :key="marker.place_id"
             :options="{ position: marker.geometry.location, ...markerOptions }" />
         </GoogleMap>
