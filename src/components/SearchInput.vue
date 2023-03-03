169dd958f46d1a212dda0f234448157ee54c3bbc
Dan Kim <itsdanya@gmail.com>
2023-03-03 23:53:36 +0400

    Fri  3 Mar 2023 23:53:36 +04

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -32,7 +32,7 @@ function onSearchResultClick(result) {
     <input type="text" name="name" id="" class="w-full px-4 h-12 border-none rounded-md bg-neutral-100" @input="onInput"
       v-model="store.query" autocomplete="off" placeholder="Start typing…">
 
-    <div v-if="store.searchResults.length" class="absolute z-10 bg-gray-50 rounded-md overflow-hidden"
+    <div v-if="store.searchResults.length" class="absolute w-full shadow-xl z-10 bg-gray-50 rounded-md overflow-hidden"
       style="top: calc(100% + 12px)">
       <div v-for="result in store.searchResults" :key="result" @click="onSearchResultClick(result)"
         class="px-4 py-3 flex flex-col hover:bg-gray-100 cursor-pointer">
