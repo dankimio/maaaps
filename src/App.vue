ae81c9a4ff2a7774d892fed90fb8f376605c274d
Dan Kim <itsdanya@gmail.com>
2023-03-07 17:52:12 +0400

    Tue  7 Mar 2023 17:52:12 +04

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,14 @@ const onEnd = (event) => {
 </script>
 
 <template>
-  <div class="container mx-auto py-12 px-4 flex flex-col min-h-screen">
-    <div class="flex-1 grid grid-cols-12 md:gap-x-12 gap-y-4 md:gap-y-0">
+  <div class="container mx-auto py-4 md:py-12 px-4 flex flex-col min-h-screen">
+    <div class="flex-1 grid grid-cols-12 md:gap-x-12 gap-y-8 md:gap-y-0">
       <div class="col-span-12 md:col-span-8">
         <MapView class="h-max" />
       </div>
 
       <div class="col-span-12 md:col-span-4">
-        <SearchInput class="mb-8" />
+        <SearchInput class="mb-4 md:mb-6" />
 
         <Sortable :list="store.markers" item-key="place_id" @end="onEnd" class="flex flex-col gap-y-4">
           <template #item="{ element, index }">
