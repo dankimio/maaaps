5b5f63edbdfb34862d47706afa9a142a6394eec4
Dan Kim <itsdanya@gmail.com>
2023-03-20 19:15:19 +0400

    Mon Mar 20 19:15:19 +04 2023

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import MapView from '../views/MapView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: LoginView
     }
   ]
 })
