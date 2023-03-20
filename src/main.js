963b03ffca5c0cd93b5399a0e12d430ea47d5e3b
Dan Kim <itsdanya@gmail.com>
2023-03-20 18:14:46 +0400

    Mon Mar 20 18:14:46 +04 2023

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import { createPinia } from 'pinia'
 import { VueFire, VueFireAuth } from 'vuefire'
 
 import './main.css'
+import router from './router'
 import App from './App.vue'
 import { firebaseApp } from './firebase'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router)
 app.use(pinia)
 app.use(VueFire, {
   firebaseApp,
