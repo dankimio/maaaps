434ab39433ecba6a8b73e25da8af81841229bbe7
Dan Kim <itsdanya@gmail.com>
2023-03-20 17:30:14 +0400

    Mon Mar 20 17:30:14 +04 2023

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyChUcdipO-eB5H4ABGHkTCqeoUf_jbzjoU',
+  authDomain: 'maaaps-4af92.firebaseapp.com',
+  databaseURL: 'https://maaaps-4af92-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'maaaps-4af92',
+  storageBucket: 'maaaps-4af92.appspot.com',
+  messagingSenderId: '703821740519',
+  appId: '1:703821740519:web:9cea697b55f1791d62e542'
+}
+
+const app = initializeApp(firebaseConfig)
