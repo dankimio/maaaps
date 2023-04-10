0043a68cd0ea6896bf5dea9ae9ec9b7cb7eb43b5
Dan Kim <itsdanya@gmail.com>
2023-04-10 19:45:18 +0100

    Mon 10 Apr 2023 19:45:18 BST

--- src/stores/map.js
+++ src/stores/map.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 
+import { placesCollection } from '../firebase'
+
 const maxZoom = 15
 
 export const useMapStore = defineStore('map', () => {
